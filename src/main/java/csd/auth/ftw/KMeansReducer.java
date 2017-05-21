77d8e2870511d2580e6c40f7e53b889c9c871673
rigopoui-kraken <Asrapogiannos-@hotmail.com>
2017-05-22 01:24:55 +0300

    changed ints to texts

--- src/main/java/csd/auth/ftw/KMeansReducer.java
+++ src/main/java/csd/auth/ftw/KMeansReducer.java
@@ -4,21 +4,21 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
+import static csd.auth.ftw.IntArrayWritable.arrayToString;
+import static csd.auth.ftw.KMeansMapper.getVectorFromLine;
 
 public class KMeansReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
     
-    public void reduce(IntWritable key, Iterable<javax.xml.soap.Text> values, Context context) throws InterruptedException, IOException {
+    public void reduce(IntWritable key, Iterable<Text> values, Context context) throws InterruptedException, IOException {
         if (context.getJobName().equals(TextClustering.KMEANS_LAST_JOB_NAME)) { // CHECK NAME FROM CONTEXT
             // TODO extract words from values and write them
         	//TODO Iterate Values extarct words put them on a String list and write them out.
         	
         	String tempString;
-        	String[] words = new String[getIteratableSize(values.iterator())];
+        	String[] words = new String[getIterableSize(values.iterator())];
         	int i =0;
                		  
         		   for(Text temp : values)
@@ -32,14 +32,17 @@ public class KMeansReducer extends Reducer<IntWritable, Text, IntWritable, Text>
             	
             context.write(key, new Text(allWords));
             // WRITE THE CENTER ID AND THE LIST OF WORDS.	
-        } else {
+        } else if (context.getJobName().equals(TextClustering.KMEANS_JOB_NAME)) {
             // TODO extract vectors and get average
             // find average
-            IntArrayWritable average = getAverage(values);
+            Text average = getAverage(values);
             
             // TODO
 //            IntArrayWritable.arrayToString(arr);
             context.write(key, average);
+        } else {
+            System.err.println("Wrong job type.");
+            // maybe throw some exception here?
         }
     }
     
@@ -55,29 +58,27 @@ public class KMeansReducer extends Reducer<IntWritable, Text, IntWritable, Text>
     }
 
 
-    private IntArrayWritable getAverage(Iterable<Text> values){
+    private Text getAverage(Iterable<Text> values){
         //getting the number of vectors in the Iterable
         int vectorNum = getIterableSize(values.iterator());
         //getting the length of the vectors
-        //Ξ―ΟƒΟ‰Ο‚ Ξ­Ο„ΟƒΞΉ Ο‡Ξ¬Ξ½ΞµΟ„Ξ±ΞΉ Ο„ΞΏ Ο€Ο�Ο�Ο„ΞΏ ΟƒΟ„ΞΏΞΉΟ‡ΞµΞ―ΞΏ, Ξ±Ξ½ Ξ· ΞΊΞ»Ξ®ΟƒΞ· Ο„Ξ·Ο‚ .iterator() Ξ΄ΞµΞ½ Ξ΄Ξ―Ξ½ΞµΞΉ ΞΊΞ±ΞΉΞ½ΞΏΟ�Ο�Ξ³ΞΉΞΏ iterator ΞΊΞ¬ΞΈΞµ Ο†ΞΏΟ�Ξ¬ (same Ξ³ΞΉΞ± Ο„Ξ·Ξ½ getIterableSize())
-        int vectorLen = values.iterator().next().get().length;
+        int vectorLen = getVectorFromLine(values.iterator().next().toString(), true).length;
         int[] averageVector = new int[vectorLen];
-        for (IntArrayWritable vector : values) {
-            IntWritable[] workingVector = vector.get();
+        for (Text textVector : values) {
+            int[] intVector = getVectorFromLine(textVector.toString(), true);
             for (int i = 0; i < vectorLen; i++) {
-                averageVector[i] += workingVector[i].get();
+                averageVector[i] += intVector[i];
             }
         }
         float tempAverage;
         for (int i = 0; i < vectorLen; i++){
             tempAverage = (float) averageVector[i]/vectorNum;
-            //Ξ±Ξ½Ξ¬ΞΈΞµΟƒΞ· ΞΌΞ­ΟƒΞ·Ο‚ Ο„ΞΉΞΌΞ®Ο‚ Ξ³ΞΉΞ± ΞΊΞ¬ΞΈΞµ Ξ΄ΞΉΞ¬ΟƒΟ„Ξ±ΟƒΞ· Ο„ΞΏΟ… ΞΌΞ­ΟƒΞΏΟ… vector (1 Ξ±Ξ½ ΞµΞ―Ξ½Ξ±ΞΉ Ο€Ξ¬Ξ½Ο‰ Ξ±Ο€Ο� 0.5, 0 Ξ±Ξ½ ΞµΞ―Ξ½Ξ±ΞΉ ΞΌΞΉΞΊΟ�Ο�Ο„ΞµΟ�ΞΏ)
             averageVector[i] = (tempAverage >= 0.5) ? 1 : 0;
         }
-        return IntArrayWritable.createFromArray(averageVector);
+        return new Text(arrayToString(averageVector));
     }
 
-    //Ξ‘Ο€Ξ»Ξ® ΞΌΞ­ΞΈΞΏΞ΄ΞΏΟ‚ Ξ³ΞΉΞ± ΞµΟ�Ο�ΞµΟƒΞ· Ο„ΞΏΟ… ΞΌΞµΞ³Ξ­ΞΈΞΏΟ…Ο‚ ΞµΞ½Ο�Ο‚ Iterable
+    //small function for finding the number of elements in an Iterable
     public static int getIterableSize(Iterator iterator){
         int i = 0;
         while(iterator.hasNext()) {
